fb4db0c770ac7dbc8730c948352c68ee82c0b369
nipunagarwal4235 <nipunagarwal2002@gmail.com>
2021-07-26 01:13:01 +0530

    Updates

---
+++ precache-manifest.8d9227fc005320d527841213eba85dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3127f00c30750556d33f41a90a21ef3",
+    "url": "/tours-react/index.html"
+  },
+  {
+    "revision": "1343a3282052d8a042ed",
+    "url": "/tours-react/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "f890e1954844aa7faa34",
+    "url": "/tours-react/static/js/2.3d099c39.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tours-react/static/js/2.3d099c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1343a3282052d8a042ed",
+    "url": "/tours-react/static/js/main.4db80f68.chunk.js"
+  },
+  {
+    "revision": "dc60b3b845f19aebdc7c",
+    "url": "/tours-react/static/js/runtime-main.27026fda.js"
+  }
+]);
